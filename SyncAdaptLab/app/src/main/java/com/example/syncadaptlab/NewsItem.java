7e6eee2be8f8337e59318c64d45b78df20c80d60
William-Tygret <wctygret@gmail.com>
2016-03-02 12:07:36 -0600

    methods

---
+++ SyncAdaptLab/app/src/main/java/com/example/syncadaptlab/NewsItem.java
@@ -0,0 +1,16 @@
+package com.example.syncadaptlab;
+
+/**
+ * Created by williamtygret on 3/2/16.
+ */
+public class NewsItem {
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ SyncAdaptLab/app/src/main/java/com/example/syncadaptlab/SearchResult.java
@@ -0,0 +1,18 @@
+package com.example.syncadaptlab;
+
+import java.util.ArrayList;
+
+/**
+ * Created by williamtygret on 3/2/16.
+ */
+public class SearchResult {
+    ArrayList<NewsItem> results;
+
+    public ArrayList<NewsItem> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<NewsItem> results) {
+        this.results = results;
+    }
+}
